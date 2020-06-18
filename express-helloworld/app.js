2b2adb19996bf379270ff7764b36052e0cf4980d
pcomeau <wmind24@hotmail.com>
2020-06-18 15:44:39 -0700

    added saturn

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -13,6 +13,10 @@ app.get('/venus', function (req, res) {
   res.send('Hello from Venus!\n');
 });
 
+app.get('/venus', function (req, res) {
+  res.send('Hello from Saturn!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
